00ff14c671293e8e0523559307fa9a40a14b7a4a
eva-barczykowska <eva.barczykowska@gmail.com>
2021-01-04 11:04:33 +0100

    small changes

--- send_sms.rb
+++ send_sms.rb
@@ -1,9 +1,9 @@
 # Download the twilio-ruby library from twilio.com/docs/libraries/ruby
 require 'twilio-ruby'
 
-# TODO remove hardcoding in the 2 below lines
-account_sid = ''
-auth_token = '' #add in the env file
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '[+][1]2059843305' # Your Twilio number
